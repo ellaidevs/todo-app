5511d1894c04c17f82deaf589203d1995e4f45cc
drakenToman <ellaidevs@gmail.com>
2021-08-21 11:00:11 +0800

    feat: add todo list

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,10 @@
 @import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap');
 
 #app {
-  /* -webkit-font-smoothing: antialiased; */
-  /* -moz-osx-font-smoothing: grayscale; */
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: hsl(234, 39%, 85%);
   font-family: 'Josefin Sans', sans-serif;
 }
 
@@ -18,6 +18,5 @@ body {
   padding: 0;
   background: hsl(235, 21%, 11%);
   font-size: 18px;
-  color: white;
 }
 </style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,24 +1,80 @@
 <template>
   <div class="container">
-    <div class="input-container">
-      <input type="checkbox" />
-      <input type="text" placeholder="Create a new todo..." />
+    <div class="header-input">
+      <div class="input-container">
+        <input type="checkbox" v-model="checkbox" />
+        <input
+          type="text"
+          placeholder="Create a new todo..."
+          v-model="todo"
+          @keyup.enter="addTodo"
+        />
+      </div>
+    </div>
+    <div class="list" v-for="todo in todoList" :key="todoList.indexOf(todo)">
+      <div>
+        <input class="list-checkbox" type="checkbox" :checked="todo.active" />
+        <input type="text" :value="todo.todo" disabled />
+      </div>
+    </div>
+    <div class="counter">
+      <div class="list">
+        <div class="clear" v-if="todoList.length > 1">
+          {{ todoList.length }} items left
+        </div>
+        <div class="clear" v-else>{{ todoList.length }} item left</div>
+        <div class="clear-completed clear">Clear completed</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  components: {},
+  data() {
+    return {
+      todoList: [],
+      todo: '',
+      checkbox: false,
+    }
+  },
+  methods: {
+    addTodo() {
+      const todoObj = {
+        todo: this.todo,
+        active: this.checkbox,
+      }
+      this.todoList.push(todoObj)
+      this.todo = ''
+      console.log(this.todoList)
+    },
+  },
+}
 </script>
 
 <style scoped>
-.container {
+.header-input,
+.list {
   display: flex;
-  border-radius: 5px;
+  border-radius: 7px;
   background: hsl(235, 24%, 19%);
   max-width: 400px;
-  /* to arrange the todo title length, check the container max-width above */
-  margin: 0 auto;
+  margin: 0 auto 20px auto;
+}
+
+input[type='text'] {
+  font-family: 'Josefin Sans', sans-serif;
+  font-size: 18px;
+  padding: 20px 20px;
+  border-style: none;
+  background: hsl(235, 24%, 19%);
+  outline: none;
+  color: hsl(234, 39%, 85%);
+}
+
+input[type='text']:hover {
+  color: hsl(236, 33%, 92%);
 }
 
 input[type='checkbox'] {
@@ -31,6 +87,7 @@ input[type='checkbox'] {
   outline: none;
   cursor: pointer;
   margin: 0 0 0 20px;
+  /* create a mobile view and reduce the left margin, */
 }
 
 input[type='checkbox']:checked {
@@ -43,13 +100,44 @@ input[type='checkbox']:checked {
   );
 }
 
-input[type='text'] {
-  font-family: 'Josefin Sans', sans-serif;
-  font-size: 18px;
-  padding: 20px 20px;
-  border-style: none;
-  background: hsl(235, 24%, 19%);
-  outline: none;
-  color: white;
+.list {
+  margin: 0 auto;
+  border-radius: 0px;
+  border-bottom: 1px solid hsl(233, 14%, 35%);
+}
+
+div.list:nth-child(2) {
+  border-radius: 7px 7px 0 0;
+
+  /* todo: add fix border-radius when only one list exist */
+}
+
+.list:nth-last-child(2) {
+  border-radius: 0 0 7px 7px;
+  border-bottom: none;
+}
+
+.counter {
+  margin: 25px auto;
+}
+
+.counter > .list {
+  display: flex;
+  color: hsl(234, 39%, 85%);
+  font-size: 14px;
+  min-height: 60px;
+  border-radius: 7px;
+}
+
+.clear {
+  margin: auto;
+}
+
+.clear-completed {
+  cursor: pointer;
+}
+
+.clear-completed:hover {
+  color: hsl(236, 33%, 92%);
 }
 </style>
